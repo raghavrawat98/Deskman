034ff3215e43974d97de4bc34725dbe8b4d5952a
Shweta <shweta.v799@gmail.com>
2022-03-12 17:45:32 +0530

    Balanced Parenthesis Test 1

--- src/CmdTextBox.js
+++ src/CmdTextBox.js
@@ -13,6 +13,68 @@ const CmdTextBox = () => {
 
         return strt;
     }
+    //component to check braces and return true
+    const checkBraces = () => {
+        console.log("---------------started check-----------------")
+        var stack = [];
+        let balanced = true;
+        for (let i = 0; i < cmdText.length; i++) {
+            let x = cmdText[i];
+            console.log(cmdText[i]);
+
+
+            if (x === '(' || x === '[' || x === '{') {
+
+                // Push the element in the stack
+                stack.push(x);
+                continue;
+            }
+
+            // If current character is not opening
+            // bracket, then it must be closing.
+            // So stack cannot be empty at this point.
+            if (stack.length === 0) {
+                //return false;
+                balanced = false;
+            }
+            let check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check === '{' || check === '[') {
+                        //return false;
+                        balanced = false;
+                        break;
+                    }
+
+                case '}':
+                    check = stack.pop();
+                    if (check === '(' || check === '[') {
+                        // return false;
+                        balanced = false;
+                        break;
+                    }
+                case ']':
+                    check = stack.pop();
+                    if (check === '(' || check === '{') {
+                        //return false;
+                        balanced = false;
+                        break;
+                    }
+            }
+        }
+
+        // Check Empty Stack
+        // return (stack.length == 0);
+        if (balanced) {
+            console.log("balanced");
+            return "Balanced";
+        }
+        console.log("Unbalanced");
+        return "Unbalanced";
+    }
+
+
     return (
         <div className="form-group">
             <input
@@ -20,11 +82,12 @@ const CmdTextBox = () => {
                 className="form-control"
                 placeholder="Input CMD"
                 onChange={e => setcmdText(e.target.value)} //Holding thr input Value and its characteristics
-                //<p> for printing Value inside textbox
+            //<p> for printing Value inside textbox
             />
 
             <p>Hi Hello</p>
-            <p>{createCMDLabel()}</p> 
+            <p>{createCMDLabel()}</p>
+            <p>{checkBraces()}</p>
         </div>
     );
 
